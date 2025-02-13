8f941ffae08f54b383a5240ab14e35c636611360
Kira Sokolova <Kyra256@proton.me>
2025-02-13 20:41:15 +0300

    refactor: move snippets into project's root

--- lua/configs/snippets.lua
+++ lua/configs/snippets.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 local config_path = vim.fn.stdpath("config")
-local snippets_vscode = config_path .. "/lua/snippets_vscode"
-local snippets_snipmate = config_path .. "/lua/snippets_snipmate"
-local snippets_lua = config_path .. "/lua/snippets_lua"
+local snippets_vscode = config_path .. "/snippets_vscode"
+local snippets_snipmate = config_path .. "/snippets_snipmate"
+local snippets_lua = config_path .. "/snippets_lua"
 
-M.luasnip = function()
+function M.luasnip()
 	require("luasnip.loaders.from_vscode").lazy_load({
 		paths = { snippets_vscode },
 	})
