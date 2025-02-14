a9fc2e06ed8c5816e6109f7986cf7fcc353c0368
Kira Sokolova <Kyra256@proton.me>
2025-02-14 16:26:13 +0300

    refactor(lspconfig): define functions with function M.name()

--- lua/mappings/lspconfig.lua
+++ lua/mappings/lspconfig.lua
@@ -14,44 +14,45 @@ require("which-key").add({
 	{ "<leader>l", group = "lsp" },
 })
 
-return {
-	--TODO: maybe set shorter keymaps?
-	setup = function(server_capabilities)
-		map("n", "<leader>lf", function()
-			require("conform").format({ async = true, lsp_fallback = true }, nil)
-		end, opts("Format"))
-		map("n", "<leader>lF", vim.diagnostic.open_float, opts("Floating diagnostics"))
+local M = {}
 
-		if server_capabilities.declarationProvider then
-			map("n", "<leader>fD", open("lsp_declarations"), opts("Declaration"))
-		end
-		if server_capabilities.definitionProvider then
-			map("n", "<leader>ld", open("lsp_definitions"), opts("Definition"))
-		end
-		if server_capabilities.hoverProvider then
-			map("n", "<leader>lk", vim.lsp.buf.hover, opts("Hover"))
-		end
-		if server_capabilities.implementationProvider then
-			map("n", "<leader>li", open("lsp_implementations"), opts("Implementation"))
-		end
-		if server_capabilities.signatureHelpProvider then
-			map("n", "<leader>ls", vim.lsp.buf.signature_help, opts("Signature help"))
-		end
-		if server_capabilities.typeDefinitionProvider then
-			map("n", "<leader>lt", open("lsp_type_definitions"), opts("Type definition"))
-		end
-		if server_capabilities.referencesProvider then
-			map("n", "<leader>lR", open("lsp_references"), opts("References"))
-		end
-		if server_capabilities.renameProvider then
-			map("n", "<leader>lr", vim.lsp.buf.rename, opts("Rename"))
-		end
-		if server_capabilities.codeActionProvider then
-			vim.keymap.set({ "v", "n" }, "<leader>la", require("actions-preview").code_actions, opts("Code actions"))
-		end
-	end,
+function M.setup(server_capabilities)
+	map("n", "<leader>lf", function()
+		require("conform").format({ async = true, lsp_fallback = true }, nil)
+	end, opts("Format"))
+	map("n", "<leader>lF", vim.diagnostic.open_float, opts("Floating diagnostics"))
 
-	outline = function()
-		map("n", "<leader>o", "<cmd>Outline!<CR>", { desc = "Toggle outline" })
-	end,
-}
+	if server_capabilities.declarationProvider then
+		map("n", "<leader>fD", open("lsp_declarations"), opts("Declaration"))
+	end
+	if server_capabilities.definitionProvider then
+		map("n", "<leader>ld", open("lsp_definitions"), opts("Definition"))
+	end
+	if server_capabilities.hoverProvider then
+		map("n", "<leader>lk", vim.lsp.buf.hover, opts("Hover"))
+	end
+	if server_capabilities.implementationProvider then
+		map("n", "<leader>li", open("lsp_implementations"), opts("Implementation"))
+	end
+	if server_capabilities.signatureHelpProvider then
+		map("n", "<leader>ls", vim.lsp.buf.signature_help, opts("Signature help"))
+	end
+	if server_capabilities.typeDefinitionProvider then
+		map("n", "<leader>lt", open("lsp_type_definitions"), opts("Type definition"))
+	end
+	if server_capabilities.referencesProvider then
+		map("n", "<leader>lR", open("lsp_references"), opts("References"))
+	end
+	if server_capabilities.renameProvider then
+		map("n", "<leader>lr", vim.lsp.buf.rename, opts("Rename"))
+	end
+	if server_capabilities.codeActionProvider then
+		vim.keymap.set({ "v", "n" }, "<leader>la", require("actions-preview").code_actions, opts("Code actions"))
+	end
+end
+
+function M.outline()
+	map("n", "<leader>o", "<cmd>Outline!<CR>", { desc = "Toggle outline" })
+end
+
+return M
